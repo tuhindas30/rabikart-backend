ffd726b4413cc29e6fde9b9135e1b2133b6ed1d1
Tuhin Das <mailme.tuhindas@gmail.com>
2021-06-10 23:08:36 +0530

    add `Bearer` keyword

--- src/middlewares/verifyAuth.js
+++ src/middlewares/verifyAuth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const secret = process.env.JWT_SECRET;
 
 const verifyAuth = (req, res, next) => {
-  const token = req.headers.authorization;
+  const token = req.headers.authorization.split(" ")[1];
   try {
     const decoded = jwt.verify(token, secret);
     req.userId = decoded.userId;
