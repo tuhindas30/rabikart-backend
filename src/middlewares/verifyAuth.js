92885461bfc88e3773d98e64b2e313bbf54336f5
Tuhin Das <mailme.tuhindas@gmail.com>
2021-07-25 12:02:28 +0530

    add more checks

--- src/middlewares/verifyAuth.js
+++ src/middlewares/verifyAuth.js
@@ -1,9 +1,19 @@
 const jwt = require("jsonwebtoken");
+const { HttpError } = require("../utils/helper");
 const secret = process.env.JWT_SECRET;
 
 const verifyAuth = (req, res, next) => {
-  const token = req.headers.authorization.split(" ")[1];
   try {
+    if (!req.headers.authorization) {
+      throw new HttpError(403, "No credentials received");
+    }
+    if (req.headers.authorization.split(" ")[0] !== "Bearer") {
+      throw new HttpError(403, "Invalid credential format");
+    }
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token) {
+      throw new HttpError(401, "Unauthorized access");
+    }
     const decoded = jwt.verify(token, secret);
     req.userId = decoded.userId;
     return next();
